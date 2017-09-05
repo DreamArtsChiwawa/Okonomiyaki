8b98a6c004cba20df2a62c71a11b9e38adc8e564
johejo <mitsuo_h@outlook.com>
2017-09-05 10:16:37 +0900

    test

---
+++ johejo/main.py
@@ -0,0 +1,11 @@
+
+def main():
+    f = open('/Users/heijo/PycharmProjects/Okonomiyaki/johejo/9.mes.utf')
+    print(f)
+    text = f.read()
+    print(text)
+    print("Hello")
+
+
+if __name__ == '__main__':
+    main()
