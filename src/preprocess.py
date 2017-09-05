c992e9b75622e19887a776f6fea9afeca430b1ff
m_heijo <mitsuo_h@outlook.com>
2017-09-06 02:33:07 +0900

    test

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,32 @@
 def main():
-    preprocessed_text = preprocess()
+    with open('../staff_wr_sample/9.mes.utf', 'r') as f:
+        raw_text = f.readlines()
+
+    preprocessed_text = preprocess(raw_text)
     print(preprocessed_text)
+
+
+def preprocess(raw_text):
+    preprocessed_text = ''
+
+    if isinstance(raw_text, str):
+        text_list = raw_text.splitlines()
+
+    else:
+        text_list = raw_text
+    #
+    # for line in text_list:
+    #
+    #
+    #     forward, backward = cut_text(raw_text, '総括')
+
+    return preprocessed_text
+
+
+# def cut_text(text, tag):
+#
+#
+#     return foward, backward
 
 
 if __name__ == '__main__':
