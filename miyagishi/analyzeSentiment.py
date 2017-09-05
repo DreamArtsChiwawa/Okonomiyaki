0fea490cba1e27f3a0f954100716209fbec70689
m_heijo <mitsuo_h@outlook.com>
2017-09-05 22:54:22 +0900

    test

--- miyagishi/analyzeSentiment.py
+++ miyagishi/analyzeSentiment.py
@@ -64,7 +64,9 @@ def analyze(text):
     # Instantiates a plain text document.
     # document = language_client.document_from_html(review_file)
     # document = language_client.document_from_html(text)
-    document = language_client.document_from_html("The quick brown fox jumps over the lazy dog.")
+    print(text)
+    exit(1)
+    # document = language_client.document_from_html("The quick brown fox jumps over the lazy dog.")
     # Detects sentiment in the document.
     annotations = document.annotate_text(include_sentiment=True,
                                          include_syntax=False,
