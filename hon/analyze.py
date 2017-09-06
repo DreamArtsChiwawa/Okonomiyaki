1389b06afba826c3a7e2d237d46c26481e2a0f54
daiking <daiking1756@gmail.com>
2017-09-06 17:24:37 +0900

    test

--- hon/analyze.py
+++ hon/analyze.py
@@ -59,7 +59,7 @@ def set_dict(annotations, text_list):
 
     n, bins, patch = plt.hist(score_list, bins=np.arange(-1.0, 1.01, 0.1))  # 度数分布表の取得   
     #plt.savefig("fig_" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".png") # ヒストグラムをファイルに出力
-    plt.savefig("fig_histgram.jpg")
+    plt.savefig("fig_histgram.png")
 
     # print('total score is {}'.format(sum_score))
     # print('average score is {}'.format(ave_score))
