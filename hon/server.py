1c4eddc55ab6cb09a78b685686e43c5ece6fd9cc
karaasJP <bskzxc@gmail.com>
2017-09-07 10:11:02 +0900

    send file

--- hon/server.py
+++ hon/server.py
@@ -29,11 +29,11 @@ def messages():
         userName = msgObj['createdUserName']
 
         flag = 0
-        
+
         if messageText.find('週報') >= 0 and messageText.find('判断') >= 0:
             send_message(companyId, groupId,"判断したい週報を入力してください！")
             flag = 1
-            
+
         if messageText.find('<< WEEKLY REPORT >>') >= 0:
             preprocessed_text = preprocess.preprocess(messageText) #テキストをAIに読みやすいようにする工程
         else:
@@ -41,14 +41,14 @@ def messages():
 
         if flag == 0:
             value = analyze.analyze(preprocessed_text)
-            #value = dammy() #ダミーの辞書を生成 
+            #value = dammy() #ダミーの辞書を生成
             return_message, return_message2, return_message3 = set_message(value) #メッセージを整形
 
             send_message(companyId, groupId, return_message) #メッセージを送信
             send_message(companyId, groupId, return_message2)
             send_message(companyId, groupId, return_message3)
-        
 
+        send_message(./ori.png)
         print("MESSEAGES SENDED") #log
 
         return "OK"
@@ -93,7 +93,7 @@ def send_file(companyId, groupId, file_path):
     url = 'https://{0}.chiwawa.one/api/public/v1/groups/{1}/files'.format(companyId, groupId)
 
     file_path = 'ori.png'
-    
+
     headers = {
 
         'X-Chiwawa-API-Token': env['CHIWAWA_API_TOKEN']
@@ -101,7 +101,7 @@ def send_file(companyId, groupId, file_path):
     }
 
 
-    image = open(file_path) 
+    image = open(file_path)
 
     headers = {
 
@@ -116,35 +116,35 @@ def send_file(companyId, groupId, file_path):
     }
 
     data = {
-    	
+
 	'filename':'ori.png'
 
     }
 
     res = requests.post(url, headers=headers, files=files, data=data)
 
-    
+
 def get_score(score):
     score = score + 1 # 0-2
     score = score * 50 # 0-100
     return int(score)
 
 def set_message(analyzed_value):
-    
+
     maxvalue = get_score(analyzed_value['max']['score'])
     minvalue = get_score(analyzed_value['min']['score'])
     totalvalue = get_score(analyzed_value['total'])
-    
+
     message = "とってもポジティブな文章は、\n「" + analyzed_value['max']['sentence'] + \
                 "」\nで、" + str(maxvalue) + "点でした！\n"
-                
-                
+
+
     message2 = "すっごくネガティブな文章は、\n「" + analyzed_value['min']['sentence'] + \
                 "」\nで、" + str(minvalue) + "点でした><\n"
-       
+
     message3 =  "ウィークリーレポートの総計は、" + str(totalvalue) + "点でした\n" \
                 "来週もがんばりましょう！！"
-        
+
     """
     message = 'RESULT' + \
               '\nMAX = ' + str(analyzed_value['max']['score']) + \
@@ -163,7 +163,7 @@ def dammy():
            'mid': {'score': 0},
            'magnitude': 11,
            'total': -4}
-    
+
     return dic
 """
 
