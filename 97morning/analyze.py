a889b414233fd9b74e550ea504d5206c13106d91
johejo <mitsuo_h@outlook.com>
2017-09-07 09:33:03 +0900

    fix

---
+++ 97morning/analyze.py
@@ -0,0 +1,83 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+from google.cloud import language
+from datetime import datetime
+
+
+def analyze(text):
+    """Run a sentiment analysis request on text within a passed filename."""
+    language_client = language.Client()
+
+    print(text)
+
+    # review_file = head.get_text(text)
+    review_file = text.replace('¥n', '\n')  # 改行文字の置き換え
+    text_list = review_file.split("\n")  # 改行文字で分かち書き
+    # print(review_file)
+    # print(text_list)
+    # with open(movie_review_filename, 'r') as review_file:
+    # Instantiates a plain text document.
+    # document = language_client.document_from_html(review_file)
+    # print(type(text))
+    # text = text.encode().decode('utf-8')
+    # print(text)
+
+    document = language_client.document_from_html(text)
+
+    annotations = document.annotate_text(include_sentiment=True,
+                                         include_syntax=False,
+                                         include_entities=False)
+
+    value_dict = set_dict(annotations, text_list)
+
+    return value_dict
+
+
+def set_dict(annotations, text_list):
+    score = annotations.sentiment.score
+    magnitude = annotations.sentiment.magnitude
+    score_list = []
+
+    for index, sentence in enumerate(annotations.sentences):
+        sentence_sentiment = sentence.sentiment.score
+        print('Sentence {} has a sentiment score of {}.The sentence is "{}"\n'.format(
+            index, sentence_sentiment, text_list[index]))
+        score_list.append(sentence_sentiment)
+
+    print('Overall Sentiment: score of {} with magnitude of {}'.format(
+        score, magnitude))
+
+    sum_score = sum(score_list)
+    ave_score = sum_score / len(score_list)
+    max_score = max(score_list)
+    min_score = min(score_list)
+    mid_score = np.median(score_list)
+    total_score = score
+
+    # print(score_list)
+    save_fig(score_list)
+
+    # print('total score is {}'.format(sum_score))
+    # print('average score is {}'.format(ave_score))
+    # print('max score is {}.The Sentence is ({})'.format(max_score, text_list[score_list.index(max_score)]))
+    # print('min score is {}'.format(min_score))
+    # print('center score is {}'.format(center_score))
+
+    dic = {'max': {'score': max_score, 'sentence': text_list[score_list.index(max_score)]},
+           'min': {'score': min_score, 'sentence': text_list[score_list.index(min_score)]},
+           'sum': sum_score,
+           'ave': ave_score,
+           'mid': {'score': mid_score},
+           'magnitude': magnitude,
+           'total': total_score}
+
+    return dic
+
+
+def save_fig(score_list):
+    file_name = "../test/fig_histgram.png"
+
+    n, bins, patch = plt.hist(score_list, bins=np.arange(-1.0, 1.01, 0.1))  # 度数分布表の取得   
+    # plt.savefig("fig_" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".png") # ヒストグラムをファイルに出力
+    plt.savefig(file_name)
---
+++ 97morning/preprocess.py
@@ -0,0 +1,114 @@
+from collections import OrderedDict
+
+# global variables
+TAGS = ['総括',
+        '課題・問題',
+        'Highlight',
+        'Other',
+        'AR',
+        '次週']
+
+IGNORE = ['◇',
+          '◆',
+          '●',
+          '○',
+          '○',
+          '==',
+          '--',
+          'ーー',
+          '＝＝',
+          ':',
+          '：',
+          '*',
+          '＊',
+          ]
+
+SENTENCE_TH = 5
+
+
+
+def main():
+    with open('../staff_wr_sample/1061.mes.utf', 'rb') as f:
+        # raw_text = f.readlines()
+        raw_text = f.read().decode()
+
+    preprocessed_text = preprocess(raw_text)
+    print(preprocessed_text)
+
+
+def preprocess(raw_text):
+
+    text_list = text2list(raw_text)
+
+    content = OrderedDict()
+
+    for tag in TAGS:
+        content[tag] = search_tag(tag, text_list)
+
+    sentences = []
+    for key, value in content.items():
+        sentence = ''
+        for v in value:
+            sentence += v
+
+        tmp = sentence.split('。')
+        tmp = remove_blank(tmp)
+        for item in tmp:
+            sentences.append(item)
+
+    result = ''
+    for s in sentences:
+        result += s + "\n"
+
+    return result
+
+
+def search_tag(tag, text_list):
+    tag_flag = 0
+    tag_content = []
+    for text in text_list:
+        if tag_flag:
+            for t in TAGS:
+                if tag_flag == 0:
+                    break
+                if text.find(t) >= 0 or (tag == t == '次週' and text.find("。") < 0 and text.find("、") < 0):
+                    tag_flag = 0
+                    continue
+            if tag_flag == 0:
+                break
+            tag_content.append(text)
+
+        if text.find(tag) >= 0:
+            tag_flag = 1
+            continue
+
+    return tag_content
+
+
+def text2list(raw_text):
+    if isinstance(raw_text, str):
+        text_list = raw_text.splitlines()
+
+    else:
+        text_list = raw_text
+
+    for i in range(len(text_list)):
+        text_list[i] = text_list[i].strip()
+
+    text_list = remove_blank(text_list)
+
+    return text_list
+
+
+def remove_blank(list_obj):
+    while True:
+        try:
+            list_obj.remove('')
+        except:
+            break
+
+    return list_obj
+
+
+if __name__ == '__main__':
+    main()
