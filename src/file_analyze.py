4ea310f931ec3d19b8306d89970fef66c4ca7112
m_heijo <mitsuo_h@outlook.com>
2017-09-08 01:01:51 +0900

    test

--- src/file_analyze.py
+++ src/file_analyze.py
@@ -25,7 +25,7 @@ def WR_analyze(WR_list):
             analyze_dict = (analyze.analyze(preprocessed_text))
         except:
             print("DICTIONARY ERROR", preprocessed_text)
-            continue
+            break
 
         week_dic[WR_name] = analyze_dict
 
